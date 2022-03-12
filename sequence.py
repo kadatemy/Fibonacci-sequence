48cfcbe904043ae7d73e09042d668bfc9988ebf6
Kadatemy <84050216+kadatemy@users.noreply.github.com>
2022-03-11 23:59:18 -0500

    initial commit

---
+++ sequence.py
@@ -0,0 +1,12 @@
+nth-term = int(input("How many terms? "))
+
+n1, n2 = 0, 1
+count = 0
+
+# Check for valid terms
+
+# Return n1 if only one term is present
+
+# If more than one, generate sequence.
+
+# Update values
